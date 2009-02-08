7d3ffe08fae01b2f0a75fed47534410d2918dbe2
Bryce Kerley <bkerley@brycekerley.net>
2009-02-08 17:33:14 -0500

    hooks for tests, also pass tests :D

--- lib/pattern/entry.rb
+++ lib/pattern/entry.rb
@@ -1,12 +1,17 @@
 module Pattern
   class Entry
+    attr_reader :args
     def initialize(args, block)
       @args = args
       @block = block
     end
     
     def match?(args)
+      return false if args.zip(@args).detect do |a|
+        !(a[1].is_a?(Wildcard)) && !(a[0] === a[1])
+      end
       
+      @block[*args]
     end
   end
 end
