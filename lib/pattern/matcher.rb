ae3f82c08f634e774bc5db26d15a7fc3df0e3d41
Bryce Kerley <bkerley@brycekerley.net>
2009-02-08 17:14:55 -0500

    matcher complete

--- lib/pattern/matcher.rb
+++ lib/pattern/matcher.rb
@@ -1,7 +1,15 @@
 module Pattern
   class Matcher
+    def initialize
+      @options = []
+    end
+    
     def match(*args, &block)
       @options << Entry.new(args, block)
+    end
+    
+    def [](*args)
+      @options.detect{|o|o.match?(*args)}
     end
   end
 end
