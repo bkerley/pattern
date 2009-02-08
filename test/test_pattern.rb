a6a9c710a6032b02923cda2a59cc5b40128bb9b8
Bryce Kerley <bkerley@brycekerley.net>
2009-02-08 17:32:41 -0500

    more tests

--- test/test_pattern.rb
+++ test/test_pattern.rb
@@ -3,13 +3,39 @@ require 'mocha'
 require File.join(File.dirname(__FILE__), '../lib/pattern')
 
 class PatternTest < Test::Unit::TestCase
+  include Pattern
+  should 'create a pattern' do
+    p = Matcher.new
+    p.match(wc) do
+    end
+    assert p.available.first.first.is_a? Wildcard
+  end
+  
+  should 'not blindly match' do
+    p = Matcher.new
+    p.match(0) do
+      assert false
+    end
+    p[1]
+  end
+  
   should 'run some pattern' do
     magazine = mock()
     magazine.expects(:ripoutallthepages).once
-    p = Pattern::Matcher.new
-    p.match(Pattern.wc) do
+    p = Matcher.new
+    p.match(0) do
       magazine.ripoutallthepages
     end
-    p[:asdf]
+    p[0]
+  end
+  
+  should 'support wildcards' do
+    magazine = mock()
+    magazine.expects(:ripoutallthepages).once
+    p = Matcher.new
+    p.match(wc) do
+      magazine.ripoutallthepages
+    end
+    p[0]
   end
 end
